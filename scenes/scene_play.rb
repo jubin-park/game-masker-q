6e15f906ff8900cb949487d12df92b6f6a0b8e1d
Jubin Park <jubin-park@naver.com>
2020-12-11 02:10:16 +0900

    add: duration time, red gradient damage frame

--- scenes/scene_play.rb
+++ scenes/scene_play.rb
@@ -11,12 +11,20 @@ class Scene_Play < Scene_Base
     SCORE_RIGHT_DAMAGE = (-50),
   ]
 
+  DAMAGE_FRAME_DURATION = 1800
+
   def initialize
     @background_image = Gosu::Image.new("images/background.png");
     @person = generate_random_person
+    @time_text = Gosu::Font.new(40, :name => "Malgun Gothic", :bold => true)
+    @time = Gosu.milliseconds + 60000
     @score_text = Gosu::Font.new(40, :name => "Malgun Gothic", :bold => true)
     @score = 0
     $bgm.play(true)
+    @damage_frame_image = Gosu::Image.new("images/damage_frame.png")
+    @damage_frame_duration = 0
+    @damage_frame_opacity = 0x0
+    @d_opacity = 10
     return
   end
 
@@ -35,11 +43,20 @@ class Scene_Play < Scene_Base
         else
           p "BAD"
           @score += SCORE_LEFT_DAMAGE
+          @damage_frame_duration = Gosu.milliseconds + DAMAGE_FRAME_DURATION
+          @damage_frame_opacity = 0x0
         end
         @person.finalize_diagnosis
       end
       @person = nil
       @person = generate_random_person
+    end
+    if Gosu.milliseconds < @damage_frame_duration
+      @d_opacity *= -1 if @damage_frame_opacity > 0xff || @damage_frame_opacity < 0
+      @damage_frame_opacity += @d_opacity
+    else
+      @damage_frame_opacity = 0
+      @d_opacity = @d_opacity.abs
     end
     return
   end
@@ -47,8 +64,17 @@ class Scene_Play < Scene_Base
   def draw
     Gosu.draw_rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0xff_eeeeee)
     @background_image.draw(0, 0, 1)
-    @score_text.draw_text_rel("Score #{@score}", WINDOW_WIDTH - 20, 20, 2, 1, 0, 1.0, 1.0, 0xff_111111)
+    t = Gosu.milliseconds
+    @time_text.draw_text_rel(sprintf("%02d:%02d.%02d", t / 60000, (t / 1000) % 60, t % 1000 / 10),
+    WINDOW_WIDTH / 2, 20, 2,
+      0.5, 0.0,
+      1.0, 1.0, 0xff_404040)
+    @score_text.draw_text_rel("Score #{@score}",
+      WINDOW_WIDTH - 20, 20, 2,
+      1.0, 0.0,
+      1.0, 1.0, 0xff_111111)
     @person.draw
+    @damage_frame_image.draw(0, 0, 99, 1.0, 1.0, ([[@damage_frame_opacity, 0x0].max, 0xff].min << 24) | 0x00_ffffff)
     return
   end
 
@@ -62,6 +88,8 @@ class Scene_Play < Scene_Base
         else
           p "BAD"
           @score += SCORE_LEFT_DAMAGE
+          @damage_frame_duration = Gosu.milliseconds + DAMAGE_FRAME_DURATION
+          @damage_frame_opacity = 0x0
         end
         @person.move_left
         @person.finalize_diagnosis
@@ -72,6 +100,8 @@ class Scene_Play < Scene_Base
         if @person.passable?
           p "BAD"
           @score += SCORE_RIGHT_DAMAGE
+          @damage_frame_duration = Gosu.milliseconds + DAMAGE_FRAME_DURATION
+          @damage_frame_opacity = 0x0
         else
           p "PERFECT"
           @score += SCORE_RIGHT_PASS
